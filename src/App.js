5f37ac1cb17ecc5a893958ddc5faf3c1783f68f2
lenochen <lenochen@foxmail.com>
2020-02-16 14:20:52 +0800

    init: :tada: 初次提交

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const MainView = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {props.children}
     </div>
-  );
+  )
 }
 
-export default App;
+const Content = props => {
+  return (
+    <div>content</div>
+  )
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainView>
+        <Header/>
+        <SideBar/>
+        <Content/>
+      </MainView>
+    )
+  }
+}
+
+export default App
